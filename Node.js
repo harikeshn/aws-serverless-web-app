c643f8336b4010109da1d733614037864f47e4e7
Harikesh N <harikeshn0003@gmail.com>
2026-01-22 11:09:55 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,8 +1,17 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
     const data = JSON.parse(event.body);
 
-    // store data in DynamoDB
-    // (we’ll add this later)
+    await dynamo.put({
+        TableName: "FeedbackTable",
+        Item: {
+            email: data.email,
+            name: data.name,
+            message: data.message
+        }
+    }).promise();
 
     return {
         statusCode: 200,
